1c44d4b5a278780a852fc866133a151813735550
Sushen Biswas <sushenbiswasaga@gmail.com>
2018-11-07 07:22:20 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import sys
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    # Web hook verification
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "hello":
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    log(data)
+
+    return "ok", 200
+
+
+def log(message):
+    print(message)
+    sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    app.run(port=80, use_reloader=True)
