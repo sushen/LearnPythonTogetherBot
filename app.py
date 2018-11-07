7295ae9e37984bb45a5865b5960ac23a14b07ab7
sushen <sushenbiswasaga@gmail.com>
2018-11-07 08:03:55 +0600

    testing prity Print

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import sys
 from flask import Flask, request
 from pprint import pprint
+from pymessenger import Bot
 
 app = Flask(__name__)
+
+FB_ACCESS_TOKEN = "EAADrJVpQGToBAHvbQvZBhgRgqjYCeSQdvobiOX6GO8bvlJ8iIR1aFIRLSVZCIbSREjObUzDB9As2ySVHZCSYWbiky2PZBCMeh2plVHEb4ZCAETuNvJXOZAoHuejEcFFQC628lzjXGAjWXZAJTgDwxXFYFvUojl4rOgZAxuK91ZATZCPQZDZD"
+bot = Bot(FB_ACCESS_TOKEN)
+
+VERIFICATION_TOKEN = "hello"
 
 
 @app.route('/', methods=['GET'])
@@ -12,7 +18,7 @@ def verify():
         if not request.args.get("hub.verify_token") == "hello":
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-    return "Hello world", 200
+    return "App Creat By Sushen Biswas To Know more Please Contact Sushen Biswas", 200
 
 
 @app.route('/', methods=['POST'])
