f78b8a90cb620e5eb5d00d535020f2934fe2bf3e
sushen <sushenbiswasaga@gmail.com>
2018-11-07 08:16:42 +0600

    testing prity Print

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ VERIFICATION_TOKEN = "hello"
 
 @app.route('/', methods=['GET'])
 def verify():
-    # Web hook verification
+    # this block of code is for Web hook verification
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == "hello":
             return "Verification token mismatch", 403
@@ -21,14 +21,36 @@ def verify():
     return "App Creat By Sushen Biswas To Know more Please Contact Sushen Biswas", 200
 
 
+# this is for posting massage to the log in json format
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
     log(data)
 
+    # Necessary Code that extract json data facebook send
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
+
+                # IDs
+                sender_id = messaging_event['sender']['id']
+                recipient_id = messaging_event['recipient']['id']
+
+                if messaging_event.get('message'):
+                    # Extracting text message
+                    if 'text' in messaging_event['message']:
+                        messaging_text = messaging_event['message']['text']
+                    else:
+                        messaging_text = 'no text'
+
+                    # Echo Bot
+                    response = messaging_text
+                    bot.send_text_message(sender_id, response)
+
     return "ok", 200
 
 
+# this for print nice log and its need pymessenger library
 def log(message):
     pprint(message)
     sys.stdout.flush()
