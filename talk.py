bf485acfedeea162aa5c96a2ec45a0b8e1542277
sushen <sushenbiswasaga@gmail.com>
2018-11-16 14:35:41 +0600

    Integrate form

--- talk.py
+++ talk.py
@@ -1,3 +1,11 @@
 def talking():
+    template = []
+    greetings ='আমি ভাল । আপনি ভাল আছেন?'
+
+    for greeting in greetings:
+        facebook_template_Dictionary = {
+                                        'title':greeting['title']
+        }
+
     return 'আমি ভাল । আপনি ভাল আছেন?'
 
