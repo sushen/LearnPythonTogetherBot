5440d1de16d2871e37edb238f9e70c334350d7eb
sushen <sushenbiswasaga@gmail.com>
2018-11-16 14:38:50 +0600

    Integrate form

--- talk.py
+++ talk.py
@@ -1,11 +1,11 @@
 def talking():
     template = []
-    greetings ='আমি ভাল । আপনি ভাল আছেন?'
+    greetings = 'আমি ভাল । আপনি ভাল আছেন?'
 
     for greeting in greetings:
         facebook_template_Dictionary = {
-                                        'title':greeting['title']
+            'title': greeting['title']
         }
+        template.append(facebook_template_Dictionary)
 
-    return 'আমি ভাল । আপনি ভাল আছেন?'
-
+    return template
