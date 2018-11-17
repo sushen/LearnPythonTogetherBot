2632b0ee3cd71c1e50e850d7c091f00714736508
sushen <sushenbiswasaga@gmail.com>
2018-11-17 09:15:20 +0600

    Integrate form

--- talk.py
+++ talk.py
@@ -1,6 +1,12 @@
 def talking():
 
-    template = [{"template_type": "generic", "elements": [ < GENERIC_TEMPLATE >, < GENERIC_TEMPLATE >, ...]}]
+    # ** How I emplement a list in function
 
+    template = [{"template_type": "generic", "elements": [  'GENERIC_TEMPLATE' ,  'GENERIC_TEMPLATE ', ...]}]
 
     return template
+
+
+# fruits = [{"template_type": "generic", "elements": [  "GENERIC_TEMPLATE",  "GENERIC_TEMPLATE" , ...]}]
+# for fruit in fruits:
+#     print(fruit)
