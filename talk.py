b13a9b67e74108bea89ed566161986b009a2f0ca
sushen <sushenbiswasaga@gmail.com>
2018-11-17 09:47:54 +0600

    Integrate Templates

--- talk.py
+++ talk.py
@@ -2,10 +2,35 @@ def talking():
 
     # ** How I emplement a list in function
 
-    templates = [{"template_type": "generic", "elements": [  'GENERIC_TEMPLATE' ,  'GENERIC_TEMPLATE ', ...]}]
+    templates = [{
+                  "recipient":{
+                    "id":"<PSID>"
+                  },
+                  "message":{
+                    "attachment":{
+                      "type":"template",
+                      "payload":{
+                        "template_type":"button",
+                        "text":"What do you want to do next?",
+                        "buttons":[
+                          {
+                            "type":"web_url",
+                            "url":"https://www.messenger.com",
+                            "title":"Visit Messenger"
+                          },
+                          {
+                            ...
+                          },
+                          {...}
+                        ]
+                      }
+                    }
+                  }
+                }]
     for template in templates:
         return template
 
+print(talking())
 
 # fruits = [{"template_type": "generic", "elements": [  "GENERIC_TEMPLATE",  "GENERIC_TEMPLATE" , ...]}]
 # for fruit in fruits:
