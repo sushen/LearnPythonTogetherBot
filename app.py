fec2ae97169655ad07ea6c4194551bb354506682
sushen <sushenbiswasaga@gmail.com>
2018-11-23 18:35:59 +0600

    import template

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-import sys
+import os, sys
 from flask import Flask, request
+from utils import wit_response, get_news_elements
+from utilsforSimpleMassage import wit_response
+from pymessenger import Bot, Element, Button
+from fbmq import Attachment, Template, QuickReply, Page
+
 from pprint import pprint
-from utils import wit_response
-from pymessenger import Bot, Element
+
+# from pymessenger2.bot import Bot
+# from pymessenger2 import Element
 
 app = Flask(__name__)
 
 # Facebook apps link:   https://developers.facebook.com/apps/2222049801408664/dashboard/
 FB_ACCESS_TOKEN = "EAAfk8UkWEJgBAIIX1hzUoRXZA4OWlh7QjLeA5nCmS9bTWzbOwyiZBUqqX9ZAWPlihNFSqBoKVtDQVx0YaR9zFtP8j8UUsLahjmvLipWAQgZBW1p6BlvMp1VlVY6INjblCUUZAAUWjJ7a4c1uGcAY6bOjPPcerFF1P2JfCKCQVdAZDZD"
+
 bot = Bot(FB_ACCESS_TOKEN)
 
 VERIFICATION_TOKEN = "hello"
@@ -20,7 +27,7 @@ def verify():
         if not request.args.get("hub.verify_token") == "hello":
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-    return "Hello world", 200
+    return "Hello World", 200
 
 
 @app.route('/', methods=['POST'])
@@ -41,54 +48,76 @@ def webhook():
                     # Extracting text message
                     if 'text' in messaging_event['message']:
                         messaging_text = messaging_event['message']['text']
+                    # add for image reply
+                    elif 'text' in messaging_event['message']:
+                        messaging_text = messaging_event['message']['attachments']
+
                     else:
                         messaging_text = 'no text'
+
+                    response = None
+                    entity, value = wit_response(messaging_text)
+
+                    if entity == 'greetings':
+                        Page.send(self(), recipient_id=sender_id, message="hello world!")
+
+    # # .........From Wit * this work and bring Generic template...........
+                    # categories = wit_response(messaging_text)
+                    # elements = get_news_elements(categories)
+                    # bot.send_generic_message(sender_id, elements)
 
                     # # Echo Bot
                     # response = messaging_text
                     # bot.send_text_message(sender_id, response)
 
-                    # greetings_response = {"payload": {
-                    #     "template_type": "button",
-                    #     "text": "What do you want to do next?",
-                    #     "buttons": [
-                    #         {
-                    #             "type": "web_url",
-                    #             "url": "https://www.messenger.com",
-                    #             "title": "Visit Messenger"
-                    #         },
-                    #
-                    #     ]
-                    # },
-                    # }
-
                     # replace Echo Bot to wit ai
-                    response = None
+                    # response = None
+                    #
+                    # entity, value = wit_response(messaging_text)
+                    #
+                    # if entity == 'greetings':
 
-                    entity, value = wit_response(messaging_text)
 
-                    if entity == 'greetings':
-                        elements = []
-                        element = Element(title="test", image_url="<arsenal_logo.png>", subtitle="subtitle",
-                                          item_url="http://arsenal.com")
-                        elements.append(element)
+    #........... for Simple text * this thing only work...........
 
-                        bot.send_generic_message(recipient_id, elements)
+                        # response = " Welcome"
+                        # bot.send_text_message(sender_id, response)
 
+
+    # # ..........For buttons * its not working ..............
+    #                     buttons = []
+    #                     button = Button(title='Arsenal', type='web_url', url='http://arsenal.com')
+    #                     buttons.append(button)
+    #                     button = Button(title='Other', type='postback', payload='other')
+    #                     buttons.append(button)
+    #                     text = 'Select'
+    #                     bot.send_button_message(recipient_id, text, buttons)
+    #                     result = bot.send_button_message(recipient_id, text, buttons)
+    #                     assert type(result) is dict
+    #                     assert result.get('message_id') is not None
+    #                     assert result.get('recipient_id') is not None
+
+    # # ..........For Image * its not working ..............
+    #                     image_url = "https://www.pexels.com/photo/nature-summer-purple-yellow-36753/"
+    #                     bot.send_image_url(recipient_id, image_url)
+
+    # # ...........For  Generic Template * its not working...........
+    #                     elements = []
+    #                     element = Element(title="test", image_url="<arsenal_logo.png>", subtitle="subtitle",
+    #                                       item_url="http://arsenal.com")
+    #                     elements.append(element)
+    #
+    #                     bot.send_generic_message(recipient_id, elements)
 
                     elif entity == 'thanks':
                         response = " Thank you too"
                         bot.send_text_message(sender_id, response)
 
                     if response == None:
-                        # response = "We are testing"
-                        # bot.send_text_message(sender_id, response)
-                        elements = []
-                        element = Element(title="test", image_url="<arsenal_logo.png>", subtitle="subtitle",
-                                          item_url="http://arsenal.com")
-                        elements.append(element)
-
-                        bot.send_generic_message(recipient_id, elements)
+                        response = "We are testing"
+                        bot.send_text_message(sender_id, response)
+                        # image_url = "https://www.pexels.com/photo/nature-summer-purple-yellow-36753/"
+                        # bot.send_image_url(recipient_id, image_url)
 
     return "ok", 200
 
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from wit import Wit
+from gnewsclient import gnewsclient
 
 access_token = "OXUVKGL5TU7HIERIGBPWEVKCFIFBLRQK"
 
@@ -7,14 +8,39 @@ client = Wit(access_token=access_token)
 
 def wit_response(message_text):
     resp = client.message(message_text)
+    categories = {'newstype': None, 'location': None}
 
-    entity = None
-    value = None
+    entities = list(resp['entities'])
+    for entity in entities:
+        categories[entity] = resp['entities'][entity][0]['value']
 
-    try:
-        entity = list(resp['entities'])[0]
-        value = resp['entities'][entity][0]['value']
-    except:
-        pass
+    return categories
 
-    return (entity, value)
+
+def get_news_elements(categories):
+    news_client = gnewsclient()
+    news_client.query = ''
+
+    if categories['newstype'] != None:
+        news_client.query += categories['newstype'] + ' '
+
+    if categories['location'] != None:
+        news_client.query += categories['location']
+
+    news_items = news_client.get_news()
+
+    elements = []
+
+    for item in news_items:
+        element = {
+            'title': item['title'],
+            'buttons': [{
+                'type': 'web_url',
+                'title': "Read more",
+                'url': item['link']
+            }],
+            'image_url': item['img']
+        }
+        elements.append(element)
+
+    return elements
